8ff439b7eed77ebda88088edffa31326dff231e6
Dima Kravchenko <d.kravchenko@paymo.ru>
2019-07-03 12:54:35 +0300

    Add not found page

---
+++ 404.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ *
+ * @package Astron
+ * @version 1.0.0
+ */
+
+get_header();
+?>
+
+    <div class="site-spacer__primary site-spacer__primary_no_indents">
+        <div class="page-status">
+            <h2 class="page-status__code slashed">
+                <span class="slashed__top" title="404"></span>
+                <span class="slashed__bottom" title="404"></span>
+            </h2>
+            <p class="page-status__msg">Smart devices to detect<br>cognitive fatigue</p>
+        </div>
+    </div>
+
+<?php
+get_footer();
